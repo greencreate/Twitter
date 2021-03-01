7fedba51a8030b9ed937b0b3c1c1c9ebdce255f6
Joey Steigelman <jhs430@nyu.edu>
2021-03-01 15:25:28 +0100

    Added user story: When composing a tweet, user sees a countdown for the number of characters remaining for the tweet (out of 280)

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -8,16 +8,33 @@
 
 import UIKit
 
-class TweetViewController: UIViewController {
+class TweetViewController: UIViewController, UITextViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tweetTextView.becomeFirstResponder()
+        tweetTextView.delegate = self
     }
     
     @IBOutlet weak var tweetTextView: UITextView!
+    @IBOutlet weak var charLabel: UILabel!
     
-    
+    func textView(_ tweetTextView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+        // Set the max character limit
+        let characterLimit = 280
+
+        // Construct what the new text would be if we allowed the user's latest edit
+        let newText = NSString(string: tweetTextView.text!).replacingCharacters(in: range, with: text)
+
+        //update character count label
+        let charCount = "\(String(newText.count))/280"
+        charLabel.text = charCount
+
+        // The new text should be allowed? True/False
+        return newText.count < characterLimit
+    }
+
     @IBAction func cancel(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
